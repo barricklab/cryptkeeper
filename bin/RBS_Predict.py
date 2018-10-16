d8a86750fe764271fb8b3cb9d0cd1abe4cdbff14
kokushkasiewicz <ecochlorotica@gmail.com>
2018-10-16 16:43:26 -0500

    Argparse arguments added to RBS predict

--- bin/RBS_Predict.py
+++ bin/RBS_Predict.py
@@ -1,28 +1,39 @@
-
-# coding: utf-8
-
-# In[37]:
-
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct  4 11:11:29 2018
 
-@authors: Alexandra Lukasiewicz and Matt McGuffi
+@authors: Alexandra Lukasiewicz and Matt McGuffie
 Purpose: Using A. Hockenberry SD sequence energy prediction, scan insert fasta 
 for potential Ribosome Binding Sites (RBS)
 """
+import argparse
 import Bio
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna
 import csv
 
 #------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='import fasta file for RBS calculation')
+parser.add_argument('-i',  
+    action='store', 
+    dest='i',
+    required=True,
+    type=str,
+    help="input file in fasta format")
 
-fasta = SeqIO.read("CMV_RNA1.fa","fasta",generic_dna)#import fasta, can make into argparse input later
+parser.add_argument('-o',  
+    action='store', 
+    dest='i',
+    required=True,
+    type=str,
+    help="prefix/title of .txt outfile")
+##------------------------------------------------------------------------------
+options = parser.parse_args()
 
-with open("json-energyRef-ACCUCCUUA.txt","r") as energies: 
+fasta = SeqIO.read(options.i,"fasta",generic_dna)#import fasta, can make into argparse input later
+
+with open("../data/json-energyRef-ACCUCCUUA.txt","r") as energies: 
     binding_energies = eval(energies.read()) #evaluates object/data structure of file
 energies.close()
 
@@ -35,16 +46,32 @@ energy = []
 
 for i in range(len(uppercase_RNA)-9):
     query = uppercase_RNA[i:i+9]
-    binding_energies[query]
-    start_site.append(i)
+    start_site.append(i+1)
     stop_site.append(i+9)
     sequence.append(query)
     energy.append(binding_energies[query])
     i += 1
     
-with open("RBS_energies.txt","w") as outfile:
+with open(options.o+'_RBS_energies_F.txt',"w") as outfile:
     writer = csv.writer(outfile, delimiter = '\t')
     writer.writerows(zip(start_site, stop_site, sequence, energy))
     outfile.close()
-    
 
+#- stranded analysis
+reverse_RNA_fasta = uppercase_RNA.reverse_complement()
+r_start_site= []
+r_stop_site = []
+r_sequence = []
+r_energy = []
+
+for i in range (len(reverse_RNA_fasta)-9):
+    query = reverse_RNA_fasta[i:i+9]
+    r_start_site.append(len(RNA_fasta)-(i+1))
+    r_stop_site.append(len(RNA_fasta)-(i+9))
+    r_sequence.append(query)
+    r_energy.append(binding_energies[query])
+    
+with open(options.o+'_RBS_energes_R.txt','w') as outfile:
+    writer = csv.writer(outfile,delimiter = '\t')
+    writer.writerows(zip(r_start_site,r_stop_site,r_sequence,r_energy))
+    outfile.close()
