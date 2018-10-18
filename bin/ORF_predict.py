c42493bfe5dcee4c58fd6ce931409b8bee7e2314
kokushkasiewicz <ecochlorotica@gmail.com>
2018-10-18 12:32:04 -0500

    added arparse options

--- bin/ORF_predict.py
+++ bin/ORF_predict.py
@@ -1,33 +1,58 @@
 #!/usr/bin/env python
 
-#import argparse
+"""
+Created on Thu Oct  18 12:26:00 2018
+
+@authors: Alexandra Lukasiewicz 
+Purpose: Predict and translate ORF regions in query. 
+Code adapted from Biopython 1.72 tutorial and cookbook, 20.1.13
+"""
+
+import argparse
 import Bio
 from Bio import SeqIO
 
-##------------------------------------------------------------------------------
-#parser = argparse.ArgumentParser(description='import fasta for slicing')
-#parser.add_argument('-i',  
-#    action='store', 
-#    dest='i',
-#    required=True,
-#    type=str,
-#    help="input .fa file to slice upstream of bTSSfinder")
-#
-#parser.add_argument('-l',  
-#    action='store', 
-#    dest='i',
-#    required=True,
-#    type=str,
-#    help="define length of ORF")
-##------------------------------------------------------------------------------
-#options = parser.parse_args()
-records = SeqIO.read("CMV_RNA1.fa","fasta")
-table = 11 #NCBI translation table for Bacterial, Archaeal, and Plant Plastids
-min_pro_len = 50 #Arbitrary number based on tutorial, can change to argparse option later
+#------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='import fasta for ORF detection')
+parser.add_argument('-i',  
+    action='store', 
+    dest='i',
+    required=True,
+    type=str,
+    help="input .fasta file for ORF search")
+
+parser.add_argument('-t',  
+    action='store', 
+    dest='t',
+    required=False,
+    default=11,
+    type=str,
+    help="set NCBI translation table, default = 11: Bacterial, Archaeal, and Plant Plastids")
+
+parser.add_argument('-l',  
+    action='store', 
+    dest='l',
+    required=False,
+    default=100,
+    type=str,
+    help="set minimum protein length of translated product")
+
+parser.add_argument('-o', 
+    action='store',
+    dest= 'o', 
+    required=True,
+    type=str,
+    help="outfile prefix")
+#------------------------------------------------------------------------------
+options = parser.parse_args()
+records = SeqIO.read(options.i,"fasta")
+table = options.t #NCBI translation table for Bacterial, Archaeal, and Plant Plastids
+min_pro_len = options.l 
+
 
 #length of sequence = multiple of 3
-if len(records.seq)%3 >0:
-    records.seq = records.seq + ((3-(len(records.seq)%3)) * "N")
+#if len(records.seq)%3 >0:
+#    records.seq = records.seq + ((3-(len(records.seq)%3)) * "N")
     
 def find_orfs_with_trans(seq, trans_table, min_protein_length):
     answer = []
@@ -56,26 +81,13 @@ def find_orfs_with_trans(seq, trans_table, min_protein_length):
     return answer
 
 orf_list = find_orfs_with_trans(records.seq, table, min_pro_len)
-print(orf_list)
-for start, end, strand, pro in orf_list:
-    print("%s...%s - length %i, strand %i, %i:%i" \
-          % (pro[:30], pro[-3:], len(pro), strand, start, end))
 
-for i in range(0,len(direction)):
-    if direction == "+":
-        seq = records.seq[sigma_one[i]:len(seq)]
-        forward_ORF = find_orfs_with_trans(seq,table,min_pro_len)
-    else:
-        revseq = records.seq[0:sigma_one[i]]
-        revseq.reverse_complement()
-
-orf_list = find_orfs_with_trans(records.seq, table, min_pro_len)
-print(orf_list)
-
-for start, end, strand, pro in orf_list:
-    with open('ORF_summary.txt','w') as outfile:
-        outfile.write("%s...%s - length %i, strand %i, %i:%i" \
-          % (pro[:30], pro[-3:], len(pro), strand, start, end))
-        outfile.close()
+#write each tuple of list to outfile
+with open(options.o +'.txt','w') as outfile:
+    outfile.write('\n'.join("%s...%s - length %i, strand %i, %i:%i" \
+    % (pro[:30], pro[-3:], len(pro), strand, start, end) for (start, end, strand, pro) in orf_list))
+    outfile.close()
+    
+    
     
     
