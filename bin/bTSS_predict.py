272ead528383e408b2924106609cf101afb9c959
kokushkasiewicz <ecochlorotica@gmail.com>
2018-10-18 11:59:01 -0500

    Added argparse arguments to script

--- bin/bTSS_predict.py
+++ bin/bTSS_predict.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#import argparse
+import argparse
 import Bio
 from Bio.SeqIO import FastaIO
 from Bio.Seq import Seq
@@ -10,24 +10,26 @@ from Bio.SeqRecord import SeqRecord
 import subprocess
 import csv
 
-##------------------------------------------------------------------------------
-#parser = argparse.ArgumentParser(description='import fasta for slicing')
-#parser.add_argument('-i',  
-#    action='store', 
-#    dest='i',
-#    required=True,
-#    type=str,
-#    help="input .fa file to slice upstream of bTSSfinder")
-#
-#parser.add_argument('-l',  
-#    action='store', 
-#    dest='i',
-#    required=True,
-#    type=str,
-#    help="define length of ORF")
-##------------------------------------------------------------------------------
-#options = parser.parse_args()
-records = SeqIO.read("CMV_RNA1.fa","fasta")
+#------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='import fasta for bTSS slicing')
+parser.add_argument('-i',  
+    action='store', 
+    dest='i',
+    required=True,
+    type=str,
+    help="input .fasta file to slice upstream of bTSSfinder")
+
+parser.add_argument('-o',  
+    action='store', 
+    dest='o',
+    required=True,
+    type=str,
+    help="output file prefix")
+
+#------------------------------------------------------------------------------
+options = parser.parse_args()
+
+records = SeqIO.read(options.i , "fasta")
 
 #prepare fasta sections of 500 bp for bTSSfinder 
 fastaSplit={} #create dictionary of sequence regions 
@@ -37,33 +39,32 @@ if len(records.seq)%250 >0:
         if n == 0:
             query_a = records.seq[n:n+500] 
             #print(len(query_a))
-            name ="CMV_RNA1_"+ str(n) +"_"+ str(n+500)
+            name =options.o+ str(n) +"_"+ str(n+500)
             seq=str(query_a)
             fastaSplit[name]=seq
             
         else:
             query_b = records.seq[n*250:n*250+500]
             #print(len(query_b))
-            name = "CMV_RNA1_"+ str(n*250)+"_"+ str(n*250+500) 
+            name = options.o+ str(n*250)+"_"+ str(n*250+500) 
             seq = str(query_b)
             fastaSplit[name]=seq
-print(fastaSplit)
-print(len(fastaSplit['CMV_RNA1_250_750']))
 
 fullFastaList = []
 for key in fastaSplit:
     fullFastaList.append(SeqRecord(Seq(fastaSplit[key]), id = key,description=""))
     
 #write fasta for bTSSfinder
-with open("RNA_summary.fasta", "w") as handle:
+with open(options.o + "_summary.fasta", "w") as handle:
     SeqIO.write(fullFastaList, handle, "fasta")
     handle.close()
 
 #run bTSSfinder 
-subprocess.Popen('bTSSfinder -i RNA1_summary.fa -o RNA1_summary -h 2', shell = True,)
+subprocess.Popen('bTSSfinder -i'+ options.o + '_summary.fasta -o'+options.o+' -h 2',
+                 shell = True)
       
 ##how to manage full .bed file summary 
-bedfile_summary = open("RNA1_summary.bed","r")
+bedfile_summary = open(options.o + "_summary.bed","r")
 lines = bedfile_summary.readlines()
 search_coords = []
 sigma_factors = []
@@ -78,33 +79,29 @@ for line in lines:
     sigma_site.append(data[11])
     direction.append(data[5])
 bedfile_summary.close()
-
-print(search_coords,sigma_factors,start_codons,sigma_site)
     
 start_coords = [i.split('_',3)[2] for i in search_coords] 
-print(start_coords)
-end_coords = [i.split('_',3)[2]for i in search_coords]
+end_coords = [i.split('_',3)[3]for i in search_coords]
 print(end_coords)
 sigma_one = [i.split(',',1)[0] for i in sigma_site]
-print(sigma_one)
 sigma_two = [i.split(',',1)[1] for i in sigma_site]
-print(sigma_two)
 
 #rectify start codon and binding sites 
+corrected_start_codon = []
+corrected_sigma_one = []
+corrected_sigma_two = []
 
 for i in range(0,len(search_coords)):
-    start_codons.append(int(start_codons[i]) + int(start_coords[i]))
-    sigma_one.append(int(sigma_one[i]) + int(start_coords[i]))
-    sigma_two.append(int(sigma_two[i]) + int(start_coords[i]))
-print(start_codons)
-print(sigma_one)
-print(sigma_two)
+    corrected_start_codon.append(int(start_codons[i]) + int(start_coords[i]))
+    corrected_sigma_one.append(int(sigma_one[i]) + int(start_coords[i]))
+    corrected_sigma_two.append(int(sigma_two[i]) + int(start_coords[i]))
+print(corrected_start_codon,corrected_sigma_one,corrected_sigma_two)
 
-
-with open('RNA1_summary_corrected.bed','w') as corrected_bed:
-    writer = csv.write(corrected_bed, delimiter = '\t', newline = '\n')
-    writer.writerows(zip(search_coords,start_coords,end_coords,sigma_factors,direction,start_codons, sigma_one,sigma_two))
-    corrected_bed.close()
+with open(options.o + '_corrected.txt','w') as corrected_bed:
+    writer = csv.writer(corrected_bed, delimiter = '\t')
+    writer.writerows(zip(search_coords,start_coords,end_coords,sigma_factors,direction,corrected_start_codon,corrected_sigma_one,corrected_sigma_two))
+corrected_bed.close()
 
     
     
+
