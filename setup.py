14b6da04992f7b374eb6719829beccb18db2873d
Cameron Roots <38054423+croots@users.noreply.github.com>
2024-05-14 11:47:55 -0500

    Update dependency information

--- setup.py
+++ setup.py
@@ -3,18 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name="cryptkeeper",
     version="0.1",
-    packages=find_packages(include=['cryptkeeper']),
-
+    packages=find_packages(include=["cryptkeeper"]),
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['ostir', 'rhotermpredict', 'promotercalculator', 'plotly', 'bokeh'],
-
+    install_requires=[
+        "ostir",
+        "rhotermpredict",
+        "promotercalculator",
+        "selenium",
+        "bokeh"
+    ],
     # metadata to display on PyPI
     author="Barrick Lab",
     author_email="jbarrick@cm.utexas.edu",
     description="Package for predicting functional components of DNA sequences",
     keywords="DNA, RNA, Cryptkeeper, Barrick, RBS, Biology, Bio",
-    url="https://github.com/barricklab/cryptkeeper",   # project home page, if any
+    url="https://github.com/barricklab/cryptkeeper",  # project home page, if any
     project_urls={
         "Bug Tracker": "https://github.com/barricklab/cryptkeeper/issues",
         "Source Code": "https://github.com/barricklab/cryptkeeper",
@@ -25,11 +29,10 @@ setup(
         "Operating System :: OS Independent",
     ],
     entry_points={
-        'console_scripts' : [
-          'cryptkeeper = cryptkeeper.cryptkeeper:main',
+        "console_scripts": [
+            "cryptkeeper = cryptkeeper.cryptkeeper:main",
         ],
     },
     include_package_data=True,
-
     # could also include long_description, download_url, etc.
 )
