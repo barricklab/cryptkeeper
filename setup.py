6eada0c7cf500229fb5cfb6cc8767aca3f78dcd2
Cameron Roots <38054423+croots@users.noreply.github.com>
2024-08-07 15:42:14 -0500

    Version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cryptkeeper",
-    version="0.1",
+    version="1.0.0",
     packages=find_packages(include=["cryptkeeper"]),
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
