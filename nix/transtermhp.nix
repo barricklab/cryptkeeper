4139c025a138276c032beaef5e5590daf6543ac2
Cameron Roots <38054423+croots@users.noreply.github.com>
2026-06-18 14:57:20 -0500

    Improve TranstermHP flake

--- nix/transtermhp.nix
+++ nix/transtermhp.nix
@@ -5,16 +5,18 @@ stdenv.mkDerivation {
   version = "2.09";
 
   src = fetchzip {
-    url = "https://transterm.cbcb.umd.edu/transterm_hp_v2.09.zip";
-    sha256 = "sha256-Zb11M15xKjNorN0A8/wG9Yg8iWKSRgSVlCqIk1XsZts=";
+    url = "https://depot.galaxyproject.org/software/transtermhp/transtermhp_2.09_src_all.zip";
+    hash = "sha256-Zb11M15xKjNorN0A8/wG9Yg8iWKSRgSVlCqIk1XsZts=";
   };
+
   patchPhase = ''
     substituteInPlace seq.cc \
       --replace "return false;" "return nullptr;"
   '';
 
   installPhase = ''
-    mkdir -p $out/bin
+    mkdir -p $out/bin $out/data
     cp transterm $out/bin/
+    cp expterm.dat $out/data/
   '';
 }
