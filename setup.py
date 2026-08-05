235d0ba997c3715d3b6d79bcda871fa8a27f5ed2
Cameron Roots <38054423+croots@users.noreply.github.com>
2026-08-05 12:52:39 -0500

    Bump version from 1.0.2 to 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cryptkeeper",
-    version="1.0.2",
+    version="1.0.3",
     python_requires=">=3.10",
     packages=find_packages(include=["cryptkeeper"]),
     # Project uses reStructuredText, so ensure that the docutils get
